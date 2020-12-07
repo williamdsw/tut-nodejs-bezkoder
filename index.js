12894c500ab876727aaa249b50d9624d73fe5b7a
William Santos <williamdsw@outlook.com>
2020-12-07 13:40:38 -0300

    Created initial config for Server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');         // Builds the Rest API
+const bodyParser = require('body-parser');  // Parses the request and create the body request
+const cors = require('cors');               // Middleware to enable CORS with options
+
+const app = express();
+
+let corsOptions = {
+    origin: 'http://localhost:8082'
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => { 
+    response.json({ message: 'Welcome to williamdsw application!' });
+});
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => { 
+    console.log(`Server is running on port ${PORT}`);
+});
